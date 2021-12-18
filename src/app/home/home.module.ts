ed485a31398003b6eb7410c03cd0524c576c8697
Christopher Parrales <lecristianot@gmail.com>
2021-12-18 17:00:14 -0500

    bugs

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -13,8 +13,8 @@ import { MessageComponentModule } from '../message/message.module';
     FormsModule,
     IonicModule,
     MessageComponentModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
